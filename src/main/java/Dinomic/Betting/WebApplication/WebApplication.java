af90f8c0a1e111927c777632582ea5dc3c5a11f0
Dinomic <hthangnguyen1101@gmail.com>
2021-03-20 11:16:53 +0700

    spring boot initialize 2

--- src/main/java/Dinomic/Betting/WebApplication/WebApplication.java
+++ src/main/java/Dinomic/Betting/WebApplication/WebApplication.java
@@ -1,4 +1,4 @@
-package Dinomic.Betting.Web;
+package Dinomic.Betting.WebApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/Dinomic/Betting/WebApplication/WebApplicationTests.java
+++ src/test/java/Dinomic/Betting/WebApplication/WebApplicationTests.java
@@ -1,4 +1,4 @@
-package Dinomic.Betting.Web;
+package Dinomic.Betting.WebApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
